1c2e73ac7c6528611623d27066d818b769200074
sherandrei <welldryus@gmail.com>
2020-10-16 15:40:07 +0300

    del jpg && red4w updt

--- test_runner.h
+++ test_runner.h
@@ -10,7 +10,7 @@
 using namespace std;
 
 template <class T>
-ostream& operator << (ostream& os, const vector<T>& s) {
+std::ostream& operator << (std::ostream& os, const std::vector<T>& s) {
     os << "{";
     bool first = true;
     for (const auto& x : s) {
@@ -24,7 +24,7 @@ ostream& operator << (ostream& os, const vector<T>& s) {
 }
 
 template <class T>
-ostream& operator << (ostream& os, const set<T>& s) {
+std::ostream& operator << (std::ostream& os, const std::set<T>& s) {
     os << "{";
     bool first = true;
     for (const auto& x : s) {
@@ -38,7 +38,7 @@ ostream& operator << (ostream& os, const set<T>& s) {
 }
 
 template <class K, class V>
-ostream& operator << (ostream& os, const map<K, V>& m) {
+std::ostream& operator << (std::ostream& os, const std::map<K, V>& m) {
     os << "{";
     bool first = true;
     for (const auto& kv : m) {
@@ -52,43 +52,43 @@ ostream& operator << (ostream& os, const map<K, V>& m) {
 }
 
 template<class T, class U>
-void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+void AssertEqual(const T& t, const U& u, const std::string& hint = {}) {
     if (t != u) {
-        ostringstream os;
+        std::ostringstream os;
         os << "Assertion failed: " << t << " != " << u;
         if (!hint.empty()) {
             os << " hint: " << hint;
         }
-        throw runtime_error(os.str());
+        throw std::runtime_error(os.str());
     }
 }
 
-void Assert(bool b, const string& hint = {}) {
+void Assert(bool b, const std::string& hint = {}) {
     AssertEqual(b, true, hint);
 }
 
 class TestRunner {
 public:
     template <class TestFunc>
-    void RunTest(TestFunc func, const string& test_name) {
+    void RunTest(TestFunc func, const std::string& test_name) {
         try {
             LOG_DURATION(" ");
             func();
-            cerr << test_name << " OK";
+            std::cerr << test_name << " OK";
         }
-        catch (exception& e) {
+        catch (std::exception& e) {
             ++fail_count;
-            cerr << test_name << " fail: " << e.what() << endl;
+            std::cerr << test_name << " fail: " << e.what() << std::endl;
         }
         catch (...) {
             ++fail_count;
-            cerr << "Unknown exception caught" << endl;
+            std::cerr << "Unknown exception caught" << std::endl;
         }
     }
 
     ~TestRunner() {
         if (fail_count > 0) {
-            cerr << fail_count << " unit tests failed. Terminate" << endl;
+            std::cerr << fail_count << " unit tests failed. Terminate" << std::endl;
             exit(1);
         }
     }
@@ -98,14 +98,14 @@ private:
 };
 
 #define ASSERT_EQUAL(x, y) {             \
-  ostringstream _local_os;               \
+  std::ostringstream _local_os;               \
   _local_os << #x << " != " << #y << ", "\
     << __FILE__ << ":" << __LINE__;      \
   AssertEqual(x, y, _local_os.str());    \
 }
 
 #define ASSERT(x) {                 \
-  ostringstream _local_os;          \
+  std::ostringstream _local_os;          \
   _local_os << #x << " is false, "  \
     << __FILE__ << ":" << __LINE__; \
   Assert(x, _local_os.str());       \
